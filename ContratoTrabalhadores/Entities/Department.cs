acaaa20bfe7090e53eaa816b097cde11f8b192b5
Gabriel Correia <gabrielcorreiajobs@hotmail.com>
2019-11-20 18:42:09 -0300

    Adicionado Entities > Enums e criado seus construtores

---
+++ ContratoTrabalhadores/Entities/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContratoTrabalhadores.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(String name)
+        {
+            Name = name;
+        }
+    }
+}
