0cde8dbd38fc4e746d9b434432011c926dfa7d6f
Gabriel Correia <gabrielcorreiajobs@hotmail.com>
2019-11-20 19:11:48 -0300

    adicionado ultimas mensagens em um for baseado pelo numero de contratos, para iniciar o calculo de valor ganho no respectivo mes escolhido pelo usuario

--- ContratoTrabalhadores/Program.cs
+++ ContratoTrabalhadores/Program.cs
@@ -22,6 +22,28 @@ namespace ContratoTrabalhadores
             Department dept = new Department(deptName);
             Worker worker = new Worker(name, level, baseSalary, dept);
 
+            Console.WriteLine("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data:");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+            }
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+            Console.WriteLine($"Name {worker.Name} \r\n Department {worker.Department.Name} \r\n Income for {monthAndYear}: {worker.Income(year, month)} ");
+
         }
     }
 }
