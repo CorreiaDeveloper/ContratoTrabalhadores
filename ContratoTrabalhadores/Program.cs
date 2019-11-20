14b60589396b212bc36a2f04bba6d9fc425adfd3
Gabriel Correia <gabrielcorreiajobs@hotmail.com>
2019-11-20 18:53:21 -0300

    Adicionado mensagens para entrar com os dados do trabalhador

--- ContratoTrabalhadores/Program.cs
+++ ContratoTrabalhadores/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ContratoTrabalhadores.Entities.Enums;
+using ContratoTrabalhadores.Entities;
+using System.Globalization;
 
 namespace ContratoTrabalhadores
 {
@@ -6,7 +9,19 @@ namespace ContratoTrabalhadores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter department's name");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base Salary:");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
         }
     }
 }
