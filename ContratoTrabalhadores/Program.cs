8fa03a55a70f00e5aca727d2d0c40acd42adeef2
Gabriel Correia <gabrielcorreiajobs@hotmail.com>
2019-11-20 18:15:04 -0300

    First Commit

---
+++ ContratoTrabalhadores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContratoTrabalhadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
